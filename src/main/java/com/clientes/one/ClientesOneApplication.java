f766fc9ec1576f000949fe8080f5244b741d63f7
Luiz <luizcb1944@hotmail.com>
2023-07-21 19:34:47 -0300

    project created

---
+++ src/main/java/com/clientes/one/ClientesOneApplication.java
@@ -0,0 +1,13 @@
+package com.clientes.one;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientesOneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientesOneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/clientes/one/ClientesOneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clientes.one;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientesOneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
