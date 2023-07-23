4f523c7a04bf0b1b32157cee96ddf6b90ccd4977
Luiz <luizcb1944@hotmail.com>
2023-07-23 18:57:38 -0300

    Method get id created

--- src/main/java/com/clientes/one/controllers/ClientesController.java
+++ src/main/java/com/clientes/one/controllers/ClientesController.java
@@ -1,9 +1,15 @@
 package com.clientes.one.controllers;
 
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -27,5 +33,21 @@ public class ClientesController {
 		BeanUtils.copyProperties(clientesRecordDto, clientesModel);		
 		return ResponseEntity.status(HttpStatus.CREATED).body(clientesRepository.save(clientesModel));
 	}
+	
+	@GetMapping("/clientes")
+	public ResponseEntity<List<ClientesModel>>getAllClientes(){
+		return ResponseEntity.status(HttpStatus.OK).body(clientesRepository.findAll());
+	}
+	
+	@GetMapping("/clientes/{id}")
+	public ResponseEntity<Object>getOneCliente(@PathVariable(value="id")UUID id,@RequestBody @Valid ClientesRecordDto clientesRecordDto){
+		Optional<ClientesModel>clientesO = clientesRepository.findById(id);
+		if(clientesO.isEmpty()) {
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Cliente não encontrado");
+		}
+		return ResponseEntity.status(HttpStatus.OK).body(clientesO.get());
+		}
+		
+		
+	}
 
-}
